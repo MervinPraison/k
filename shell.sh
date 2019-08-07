a34f04aab99e9d24c90efc230d5f0e580ff9a0a5
MervinPraison <praison.ms@gmail.com>
2019-08-07 11:25:14 +0100

    update

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@ for item in $(kaggle kernels list --user mervinpraison); do
 	if [[ $item == *"mervinpraison"* ]]; then
 		toreplace=""
 		echo "${item/mervinpraison\/$toreplace}"
-		kaggle kernels pull $item -p "${item/mervinpraison\/$toreplace}" 		
+		kaggle kernels output $item -p "${item/mervinpraison\/$toreplace}" 		
 	fi 
 done;
 git add .;
